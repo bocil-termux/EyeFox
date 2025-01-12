df71330d08f0b16cc9034984e68caa8b7b5ca359
bocil-termux <pentesting833@gmail.com>
2025-01-12 21:37:12 +0800

    Update setup_tool.py

--- setup_tool.py
+++ setup_tool.py
@@ -2,7 +2,6 @@ import os
 import subprocess
 
 def unzip_and_setup():
-    # Langkah 1: Ekstrak file ZIP
     source_folder = '.main/tools/'
     if os.path.exists(source_folder):
         for zip_file in os.listdir(source_folder):
@@ -10,17 +9,14 @@ def unzip_and_setup():
                 with zipfile.ZipFile(os.path.join(source_folder, zip_file), 'r') as zip_ref:
                     zip_ref.extractall(source_folder)
 
-    # Langkah 2: Ubah permission file dengan chmod +x
     os.system("chmod +x .main/* > /dev/null 2>&1")
     os.system("chmod +x .main/tools/* > /dev/null 2>&1")
 
-    # Langkah 3: Instal paket menggunakan apt
     subprocess.run([
         "sudo", "apt", "install", "-y", 
         "python3-cryptography", "python3-ntplib", "python3-colorama"
     ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-    # Langkah 4: Cetak selesai
     print("Selesai")
 
 if __name__ == "__main__":
