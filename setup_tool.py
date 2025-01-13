7ab75d07913c076fce243e0b1ea8908e2981c5a6
bocil-termux <pentesting833@gmail.com>
2025-01-13 09:36:58 +0800

    Update setup_tool.py

--- setup_tool.py
+++ setup_tool.py
@@ -1,8 +1,13 @@
 import os
 import subprocess
+import zipfile
 
 def unzip_and_setup():
     source_folder = '.main/tools/'
+
+    gf_dir = os.path.expanduser("~/.gf")
+    os.makedirs(gf_dir, exist_ok=True)
+
     if os.path.exists(source_folder):
         for zip_file in os.listdir(source_folder):
             if zip_file.endswith('.zip'):
@@ -13,14 +18,16 @@ def unzip_and_setup():
     os.system("chmod +x .main/tools/* > /dev/null 2>&1")
 
     subprocess.run([
-        "sudo", "apt", "install", "-y", 
-        "python3-cryptography", "toilet", "toilet-fonts",  "python3-ntplib", "python3-colorama"
+        "sudo", "apt", "install", "-y",
+        "python3-cryptography", "toilet", "toilet-fonts", "python3-ntplib", "python3-colorama"
     ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-    subprocess.run(["unzip", "gf-pattern.zip", "-d", "~/.gf/"])
+    zip_file_path = "gf-pattern.zip"
+    if os.path.exists(zip_file_path):
+        with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+            zip_ref.extractall(gf_dir)
 
     print("Selesai")
 
 if __name__ == "__main__":
-    import zipfile
     unzip_and_setup()
